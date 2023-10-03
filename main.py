3111182633384150ec01c4a572c90ff38de36993
Arman baig <armanbaig012@gmail.com>
2023-10-03 19:07:43 +0530

    Fixed Sound effects and reformatted code

--- main.py
+++ main.py
@@ -8,90 +8,105 @@ from menu import Menu
 # Initialize pygame
 pygame.init()
 
-# Set up the window
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Pong')
-clock = pygame.time.Clock()
-running = True
-dt = 0
 
-# Load sound effects
-bounce_sound = pygame.mixer.Sound('bounce.wav')
-score_sound = pygame.mixer.Sound('score.wav')
+# Function to initialize the game
+def initialize_game():
+    # Set up the window
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Pong')
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
 
-paddle1_pos = pygame.Vector2(10, screen.get_height() / 2 - 50)
-paddle2_pos = pygame.Vector2(screen.get_width() - 30, screen.get_height() / 2 - 50)
-ball_pos = pygame.Vector2(screen.get_width() / 2 - 200, screen.get_height() / 2 - 200)
-ball_velocity = pygame.Vector2(1, 1)
+    # Load sound effects
+    bounce_sound = pygame.mixer.Sound('sounds/bounce.wav')
+    score_sound = pygame.mixer.Sound('sounds/score.wav')
 
-# main menu
-menu = Menu(screen)
-menu.draw()
-menu.hold()
+    # Initialize positions and velocities
+    paddle1_pos = pygame.Vector2(10, screen.get_height() / 2 - 50)
+    paddle2_pos = pygame.Vector2(screen.get_width() - 30, screen.get_height() / 2 - 50)
+    ball_pos = pygame.Vector2(screen.get_width() / 2 - 200, screen.get_height() / 2 - 200)
+    ball_velocity = pygame.Vector2(1, 1)
 
-scoreboard = ScoreBoard(screen)  # Create ScoreBoard instance outside the game loop
-line = Line(screen)
-line.draw()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill("black")
+    # Create objects
+    menu = Menu(screen)
+    scoreboard = ScoreBoard(screen)
     line = Line(screen)
-    line.draw()
+
+    # Initialize paddles and ball
     paddle1 = Paddle(screen, paddle1_pos)
-    paddle1.draw()
     paddle2 = Paddle(screen, paddle2_pos)
-    paddle2.draw()
-
     ball = Ball(screen, ball_pos, ball_velocity)
-    ball.draw()
-    ball.move(dt)
 
-    # check collision with top and bottom walls
+    return screen, clock, running, dt, bounce_sound, score_sound, \
+        paddle1, paddle2, ball, menu, scoreboard, line
 
-    if ball.pos.y >= screen.get_height()-10 or ball.pos.y <= 10:
-        ball.bounce_y()
-        bounce_sound.play()  # Play bounce sound effect
 
-    # conditions to check collision with paddles
-    if 40 >= ball.pos.x > 20 and 0 <= ball.pos.y - paddle1_pos.y < 100:
-        ball.pos.x = 40
-        ball.bounce_x()
-        bounce_sound.play()  # Play bounce sound effect
+# Main game loop
+def main():
+    screen, clock, running, dt, bounce_sound, score_sound, \
+        paddle1, paddle2, ball, menu, scoreboard, line = initialize_game()
 
-    if screen.get_width() - 40 <= ball.pos.x < screen.get_width() - 20 and 0 <= ball.pos.y - paddle2_pos.y < 100:
-        ball.pos.x = screen.get_width() - 40
-        ball.bounce_x()
-        bounce_sound.play()  # Play bounce sound effect
+    menu.draw()
+    menu.hold()
 
-    # conditions to check if ball is out of bounds
-    if ball.pos.x >= 800:
-        scoreboard.l_point()
-        ball.reset_pos()
-        score_sound.play()  # Play score sound effect
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    if ball.pos.x <= 0:
-        scoreboard.r_point()
-        ball.reset_pos()
-        score_sound.play()  # Play score sound effect
+        screen.fill("black")
+        line.draw()
+        paddle1.draw()
+        paddle2.draw()
+        ball.draw()
+        ball.move(dt)
 
-    keys = pygame.key.get_pressed()
+        # Check collision with top and bottom walls
+        if ball.pos.y >= screen.get_height() - 10 or ball.pos.y <= 10:
+            ball.bounce_y()
+            bounce_sound.play()  # Play bounce sound effect
 
-    if keys[pygame.K_w]:
-        paddle1.go_up(dt)
-    if keys[pygame.K_s]:
-        paddle1.go_down(dt)
-    if keys[pygame.K_UP]:
-        paddle2.go_up(dt)
-    if keys[pygame.K_DOWN]:
-        paddle2.go_down(dt)
+        # Conditions to check collision with paddles
+        if 40 >= ball.pos.x > 20 and 0 <= ball.pos.y - paddle1.player_pos.y < 100:
+            ball.pos.x = 40
+            ball.bounce_x()
+            bounce_sound.play()  # Play bounce sound effect
 
-    scoreboard.update()
+        if screen.get_width() - 40 <= ball.pos.x < screen.get_width() - 20 and 0 <= ball.pos.y - paddle2.player_pos.y < 100:
+            ball.pos.x = screen.get_width() - 40
+            ball.bounce_x()
+            bounce_sound.play()  # Play bounce sound effect
 
-    pygame.display.flip()
-    dt = clock.tick(2000) / 2000
+        # Conditions to check if ball is out of bounds
+        if ball.pos.x >= 800:
+            scoreboard.l_point()
+            ball.reset_pos()
+            score_sound.play()  # Play score sound effect
 
-pygame.quit()
+        if ball.pos.x <= 0:
+            scoreboard.r_point()
+            ball.reset_pos()
+            score_sound.play()  # Play score sound effect
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            paddle1.go_up(dt)
+        if keys[pygame.K_s]:
+            paddle1.go_down(dt)
+        if keys[pygame.K_UP]:
+            paddle2.go_up(dt)
+        if keys[pygame.K_DOWN]:
+            paddle2.go_down(dt)
+
+        scoreboard.update()
+
+        pygame.display.flip()
+        dt = clock.tick(2000) / 2000
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
